cb42ff7b5b54bf7c0fa056156341611a4f5ebd85
Moon Yun-gi <yg3988@gmail.com>
2019-04-05 20:10:52 +0900

    강사페이지 버튼

--- src/components/class/board-nav.jsx
+++ src/components/class/board-nav.jsx
@@ -1,5 +1,5 @@
 //node_modules
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 
@@ -8,19 +8,40 @@ import styles from 'components/class/board-nav.css';
 
 const cx = classNames.bind(styles);
 
-const BoardNavigator = () => {
+class BoardNavigator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isTeacher : false
+        };
+    }
+    render() {
+        const isTeacher = this.state.isTeacher;
 
-    return (
-        <div className={cx("board-nav-container")}>
-            <div className={cx("board-nav-body")}>
-                <div id="aboutMenu"><p>소개</p></div>
-                <div id="noticeMenu"><p>공지사항</p></div>
-                <div id="curriMenu"><p>학사과정/교과</p></div>
-                <div id="deptMenu"><p>학과소식</p></div>
-                <div id="boardMenu"><p>게시판</p></div>
-            </div>        
+        let menu = null;
+        if(isTeacher) {
+            menu = <div className={cx("board-nav-body","board-nav-body-included-teacherMenu")}>
+            <div id="noticeMenu"><p>공지사항</p></div>
+            <div id="homeworkMenu"><p>과제</p></div>
+            <div id="qnaMenu"><p>Q&A</p></div>
+            <div id="quizMenu"><p>LiveQuiz</p></div>
+            <div id="teacherMenu"><p>강사페이지</p></div>
         </div>
-    );
+        }else{
+            menu = <div className={cx("board-nav-body")}>
+            <div id="noticeMenu"><p>공지사항</p></div>
+            <div id="homeworkMenu"><p>과제</p></div>
+            <div id="qnaMenu"><p>Q&A</p></div>
+            <div id="quizMenu"><p>LiveQuiz</p></div>
+        </div>
+        }
+
+        return (
+            <div className={cx("board-nav-container")}>
+                {menu}
+            </div>
+        );
+    }
 }
  
 export default BoardNavigator;
---
+++ src/components/class/boards.jsx
@@ -0,0 +1,25 @@
+//node_module
+import React, { Component } from 'react';
+import { classNames } from "classnames/bind";
+//stylesheet
+import style from "components/class/boards.css";
+
+const cx = classNames.bind(style);
+
+class Boards extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() { 
+        return (
+            <div>
+                hi
+            </div>
+        );
+    }
+}
+ 
+export default Boards;
