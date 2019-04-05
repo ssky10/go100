4a66b1ab8a0da65c47bc442f9cc7955f1ee44c6d
ssky10 <ch3644101@naver.com>
2019-04-05 23:00:23 +0900

    게시판 바 목록 개수와 상관없이 배경 맞춤 - css에서 height값 일부 변경

--- src/components/class/board-nav.jsx
+++ src/components/class/board-nav.jsx
@@ -1,47 +1,47 @@
 //node_modules
-import React, { Component } from 'react';
-import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 
 //stylesheet
-import styles from 'components/class/board-nav.css';
+import styles from "components/class/board-nav.css";
 
 const cx = classNames.bind(styles);
 
 class BoardNavigator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isTeacher : false
-        };
-    }
-    render() {
-        const isTeacher = this.state.isTeacher;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTeacher: true
+    };
+  }
+  render() {
+    const isTeacher = this.state.isTeacher;
 
-        let menu = null;
-        if(isTeacher) {
-            menu = <div className={cx("board-nav-body","board-nav-body-included-teacherMenu")}>
-            <div id="noticeMenu"><p>공지사항</p></div>
-            <div id="homeworkMenu"><p>과제</p></div>
-            <div id="qnaMenu"><p>Q&A</p></div>
-            <div id="quizMenu"><p>LiveQuiz</p></div>
-            <div id="teacherMenu"><p>강사페이지</p></div>
-        </div>
-        }else{
-            menu = <div className={cx("board-nav-body")}>
-            <div id="noticeMenu"><p>공지사항</p></div>
-            <div id="homeworkMenu"><p>과제</p></div>
-            <div id="qnaMenu"><p>Q&A</p></div>
-            <div id="quizMenu"><p>LiveQuiz</p></div>
-        </div>
-        }
-
-        return (
-            <div className={cx("board-nav-container")}>
-                {menu}
+    return (
+      <div className={cx("board-nav-container")}>
+        <div className={cx("board-nav-body")}>
+          <div id="noticeMenu">
+            <p>공지사항</p>
+          </div>
+          <div id="homeworkMenu">
+            <p>과제</p>
+          </div>
+          <div id="qnaMenu">
+            <p>Q&A</p>
+          </div>
+          <div id="quizMenu">
+            <p>LiveQuiz</p>
+          </div>
+          {isTeacher && (
+            <div id="teacherMenu">
+              <p>강사페이지</p>
             </div>
-        );
-    }
+          )}
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default BoardNavigator;
