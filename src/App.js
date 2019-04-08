a3fa45f4a0fd8c05fe5e30980d14ff876ebb2d1c
ssky10 <ch3644101@naver.com>
2019-04-08 16:30:21 +0900

    폴더 명칭 복수형으로 통일

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 //node_module
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 //container&components
-import { Login, ClassesList, Class, Exam } from 'container';
+import { Login, ClassesList, Class, Exam } from "containers";
 //stylesheet
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={ Login }/>
-          <Route exact path="/classeslist" component={ ClassesList }/>
-          <Route exact path="/class" component={ Class }/>
-          <Route exact path="/exam" component={ Exam } />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/classeslist" component={ClassesList} />
+          <Route exact path="/class" component={Class} />
+          <Route exact path="/exam" component={Exam} />
         </div>
       </BrowserRouter>
     );
