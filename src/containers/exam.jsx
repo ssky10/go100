c0b28c062076f5fea04c1663909ecac394a7d1ab
ssky10 <ch3644101@naver.com>
2019-04-30 01:03:20 +0900

    material-ui를 이용한 시험 페이지 레이아웃 1차 구성

--- src/containers/exam.jsx
+++ src/containers/exam.jsx
@@ -1,11 +1,26 @@
 //node_modules
 import React, { Component } from "react";
 import classNames from "classnames/bind";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+
+//components
+import Navigator from "components/exam/navigator";
 
 //stylesheet
 import style from "containers/exam.module.css";
-
 const cx = classNames.bind(style);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fafafa", //"#FEDBD0",
+      contrastText: "#000000" //"#442C2E"
+    },
+    secondary: {
+      main: "#feeae6"
+    }
+  }
+});
 
 class Exam extends Component {
   constructor(props) {
@@ -13,7 +28,19 @@ class Exam extends Component {
     this.state = {};
   }
   render() {
-    return <div>exam page</div>;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className={cx("class-container")}>
+          <div className={cx("navigator")}>
+            <Navigator />
+          </div>
+          <div className={cx("main-body")}>
+            <div className={cx("board-menu")} />
+            <div className={cx("board")} />
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
