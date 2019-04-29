ef66e280694170f1c508d322e23e303c39618551
Moon Yun-gi <yg3988@gmail.com>
2019-04-30 08:34:29 +0900

    redux설정 끝

--- src/components/class/navigator.jsx
+++ src/components/class/navigator.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+
 import style from './navigator.module.css';
 
 const cx = classNames.bind(style);
 
-const Navigator = () => {
+const Navigator = ({onChangeBoard}) => {
     return (
         <div className={cx('nav-header')}>
             <h2 className={cx('logo')}>
                 Go100
             </h2>
-            <input type="checkbox" className={cx('chk')}/>
+            <input type="checkbox" id="chk" className={cx('chk')}/>
             <label htmlFor="chk" className={cx('show-menu-btn')}>
                 <i><FontAwesomeIcon icon="bars"/></i>
             </label>
             <ul className={cx('menu')}>
-                <a href="#">Home</a>
+                <a
+                    onClick={()=>onChangeBoard(0)}
+                >
+                Home
+                </a>
                 <a href="#">About</a>
                 <label htmlFor="chk" className={cx('hide-menu-btn')}>
                     <i><FontAwesomeIcon icon="times"/></i>
@@ -25,5 +31,12 @@ const Navigator = () => {
         </div>
     );
 }
- 
+
+Navigator.propTypes = {
+  onChangeBoard: PropTypes.func
+}
+
+Navigator.defaultProps = {
+  onChangeBoard: () => console.warn('onChangeBoard not defined')
+}
 export default Navigator;
