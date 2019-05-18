c3db2669aa495e3f23f4c318d67b1cc4aeb4c1b4
ssky10 <ch3644101@naver.com>
2019-05-19 00:45:06 +0900

    문제 검색창 추가

--- src/components/exam/examBoard.jsx
+++ src/components/exam/examBoard.jsx
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import SearchIcon from "@material-ui/icons/Search";
+import Grid from "@material-ui/core/Grid";
 
 const styles = theme => ({
   content: {
@@ -10,32 +14,94 @@ const styles = theme => ({
     padding: theme.spacing.unit * 3
   },
   paper: {
-    height: "100%"
+    height: "100%",
+    padding: theme.spacing.unit * 1,
+    marginBottom: theme.spacing.unit * 1
+  },
+  title: {
+    margin: 0
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.black, 0.25)
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit,
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 5,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 5,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
   }
 });
 
-const ExamBoard = ({ subject, classes }) => {
+const ExamBoard = ({ subject, classes, question }) => {
+  // if (!document.getElementById("jqMath")) {
+  //   const scriptjqMath = document.createElement("script");
+  //   scriptjqMath.id = "Mathjax";
+  //   scriptjqMath.src =
+  //     "//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML";
+  //   document.body.appendChild(scriptjqMath);
+  // }
+
   return (
     <main className={classes.content}>
       <Paper className={classes.paper} elevation={1}>
-        {parseInt(subject / 100) === 0 && (
-          <Typography paragraph>국어</Typography>
-        )}
-        {parseInt(subject / 100) === 1 && (
-          <Typography paragraph>영어</Typography>
-        )}
-        {parseInt(subject / 100) === 2 && (
-          <Typography paragraph>수학</Typography>
-        )}
-        {parseInt(subject / 100) === 3 && (
-          <Typography paragraph>한국사</Typography>
-        )}
-        {parseInt(subject / 100) === 4 && (
-          <Typography paragraph>사회</Typography>
-        )}
-        {parseInt(subject / 100) === 5 && (
-          <Typography paragraph>과학</Typography>
-        )}
+        <Grid container spacing={8}>
+          <Grid item xs={9}>
+            <Typography className={classes.title} variant="h4" gutterBottom>
+              {subject}
+            </Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="문제번호"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Paper className={classes.paper} elevation={1}>
+        <Typography className={classes.title} variant="h6" gutterBottom>
+          {subject}
+        </Typography>
       </Paper>
     </main>
   );
@@ -43,9 +109,8 @@ const ExamBoard = ({ subject, classes }) => {
 
 ExamBoard.propTypes = {
   classes: PropTypes.object.isRequired,
-  // Injected by the documentation to work in an iframe.
-  // You won't need it on your project.
-  subject: PropTypes.number
+  subject: PropTypes.string,
+  question: PropTypes.string
 };
 
 export default withStyles(styles, { withTheme: true })(ExamBoard);
