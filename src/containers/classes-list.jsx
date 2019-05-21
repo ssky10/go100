6f621186e83fa262787932367e97ac819871898d
ssky10 <ch3644101@naver.com>
2019-05-21 18:08:00 +0900

    merge 오류 해결 classes-list 부분 merge 에러

--- src/containers/classes-list.jsx
+++ src/containers/classes-list.jsx
@@ -1,6 +1,5 @@
 //node_modules
 import React, { Component } from "react";
-import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import List from "@material-ui/core/List";
@@ -10,68 +9,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 //components
 import Template from "components/template";
 
-//stylesheet
-// import styles from "containers/classes-list.module.css";
-import { withStyles } from "@material-ui/core";
-
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-
-const styles = theme => ({
-  appBar: {
-    position: 'relative',
-  },
-  icon: {
-    marginRight: theme.spacing.unit * 2,
-  },
-  heroUnit: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  heroContent: {
-    maxWidth: 600,
-    margin: '0 auto',
-    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
-  },
-  heroButtons: {
-    marginTop: theme.spacing.unit * 4,
-  },
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  cardGrid: {
-    padding: `${theme.spacing.unit * 8}px 0`,
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing.unit * 6,
-  },
-});
-
 import ClassList from "components/class/classeslist";
-const cx = classNames.bind(styles);
 
 class ClassesList extends Component {
   constructor(props) {
@@ -80,43 +18,29 @@ class ClassesList extends Component {
   }
 
   render() {
-    const { classes, theme, isLogin, user} = this.props;
+    const { classes, theme, isLogin, user } = this.props;
     // const { classes }= this.props;
 
     return (
-      <div classname = {classes}>
+      <div classname={classes}>
+        <Template theme={theme} title="클래스 목록">
+          <div>
+            {isLogin && <h1>{user}의 클래스 목록입니다.</h1>}
+            <List component="nav">
+              <ListItem button component={Link} to="class">
+                <ListItemText primary="Class Page" />
+              </ListItem>
+              <ListItem button component={Link} to="exam">
+                <ListItemText primary="Exam" />
+              </ListItem>
+            </List>
+          </div>
+        </Template>
 
-
-      <Template theme={theme} title="클래스 목록">
         <div>
-          {isLogin && <h1>{user}의 클래스 목록입니다.</h1>}
-          <List component="nav">
-            <ListItem button component={Link} to="class">
-              <ListItemText primary="Class Page" />
-            </ListItem>
-            <ListItem button component={Link} to="exam">
-              <ListItemText primary="Exam" />
-            </ListItem>
-          </List>
+          <ClassList />
         </div>
-      </Template>
-
-      <div>
-        <ClassList>
-          
-        </ClassList>
       </div>
-      
-
-
-
-      
-
-
-
-
-
-     </div>
     );
   }
 }
@@ -132,5 +56,5 @@ const mapDispatchToProps = dispatch => ({});
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(ClassesList);
