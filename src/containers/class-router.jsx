daabd9f0207285317e8b2b40d1134f2fc9c9e698
Moon Yun-gi <yg3988@gmail.com>
2019-06-02 22:03:34 +0900

    modify container class-router.jsx

--- src/containers/class-router.jsx
+++ src/containers/class-router.jsx
@@ -3,6 +3,8 @@ import { Route, Switch, Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { ListItem, ListItemIcon, ListItemText, Divider, SvgIcon } from '@material-ui/core'
 
+
+
 import {
     MainBoard, 
     NoticeBoard, 
@@ -11,12 +13,12 @@ import {
     LiveQuizBoard,
     TeacherBoard
 } from "containers/board";
+import { TemplateContainer } from "containers";
 
 import QnAPost from 'components/class/board-contents/qnaboard/qna-post';
 import QnAWrite from 'components/class/board-contents/qnaboard/qna-write';
 
-import { TemplateContainer } from "containers";
-import { useAuth } from "context/loginProvider";
+import { useAuth } from 'context/loginProvider';
 
 import HomeIcon from "@material-ui/icons/Home";
 import NoticeIcon from "@material-ui/icons/Announcement";
@@ -32,6 +34,7 @@ class ClassRouter extends Component {
             isTeacher: true
         }
     }
+
     render() { 
         const { theme, match, token } = this.props;
         console.log(this.props);
@@ -50,11 +53,11 @@ class ClassRouter extends Component {
             <TeacherIcon />
         ];
 
-        const ListItems = (text, index, URLs=["/class","/class/notice","/class/work","/class/qna","/class/livequiz","/class/teacher"]) => (
+        const ListItems = (text, index, URLs=["","/notice","/work","/qna","/livequiz","/teacher"]) => (
 			<ListItem 
 				button
                 component={Link}
-                to={`${URLs[index]}`}
+                to={`${match.url}${URLs[index]}`}
             >
                 <ListItemIcon>
                 <SvgIcon>{boardIcons[index]}</SvgIcon>
@@ -75,8 +78,7 @@ class ClassRouter extends Component {
                 </div>
               ))}
             </div>
-          );
-
+        );
         return (
             <TemplateContainer 
                 theme={theme}
@@ -87,23 +89,24 @@ class ClassRouter extends Component {
             >
                 <Switch>
                     <Route exact path={match.url} render={()=>(
-                        <MainBoard boardIdx={0}/>
+                        <MainBoard boardIdx={0} classIdx={match.params.id} token={token}/>
                     )}/>
-                    <Route path="/class/notice" render={()=>(
-                        <NoticeBoard boardIdx={1}/>
+                    <Route path={`${match.url}/notice`} render={()=>(
+                        <NoticeBoard boardIdx={1} token={token}/>
+                    )}/>
+                    <Route path={`${match.url}/work`} render={()=>(
+                        <WorkBoard boardIdx={2} token={token}/>
                     )}/>
                     <Route exact path={`${match.url}/qna`} render={()=>(
                         <QnABoard boardIdx={3} classIdx={match.params.id} token={token}/>
                     )}/>
-                    <Route exact path="/class/qna" render={()=>(
-                        <QnABoard boardIdx={3}/>
-                    )}/>   
-                    <Route path="/class/qna/post/:id" component={QnAPost}/>
-                    <Route path="/class/livequiz" render={()=>(
-                        <LiveQuizBoard boardIdx={4}/>
+                    <Route path={`${match.url}/qna/write`} component={QnAWrite}/>
+                    <Route path={`${match.url}/qna/post/:id`} component={QnAPost}/>
+                    <Route path={`${match.url}/livequiz`} render={()=>(
+                        <LiveQuizBoard boardIdx={4}  token={token}/>
                     )}/>
-                    <Route path="/class/teacher" render={()=>(
-                        <TeacherBoard boardIdx={5}/>
+                    <Route path={`${match.url}/teacher`} render={()=>(
+                        <TeacherBoard boardIdx={5}  token={token}/>
                     )}/> 
                 </Switch>
             </TemplateContainer>            
