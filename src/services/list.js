84c8410ef6b0c825a0030b596fbf00fe2b77ce46
ssky10 <ch3644101@naver.com>
2019-06-02 18:17:08 +0900

    클래스 생성 기능 추가

--- src/services/list.js
+++ src/services/list.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
 export function getlist(token) {
-    return axios.post("https://golony.dev/api/classroom/list", {
-      Origin: window.location.hostname,
-      user_token: token
-    });
-  }
+  return axios.post("https://golony.dev/api/classroom/list", {
+    Origin: window.location.hostname,
+    user_token: token
+  });
+}
+
+export function makeClass(token, name, desc) {
+  return axios.post("https://golony.dev/api/classroom/newclass", {
+    Origin: window.location.hostname,
+    user_token: token,
+    name: name,
+    desc: desc
+  });
+}
