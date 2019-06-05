8930dbd17e3a3627b26e8be0922ee7ac5ea82709
ssky10 <ch3644101@naver.com>
2019-06-06 03:32:07 +0900

    merge 누락 해결

--- src/containers/board/index.jsx
+++ src/containers/board/index.jsx
@@ -5,4 +5,5 @@ export { default as QnABoard } from "containers/board/qnaboard/qnaboard";
 export { default as QnAPost } from "containers/board/qnaboard/qna-post";
 export { default as QnAWrite } from "containers/board/qnaboard/qna-write";
 export { default as LiveQuizBoard } from "containers/board/livequizboard/livequizboard";
+export { default as LiveQuiz } from "containers/board/livequizboard/livequiz";
 export { default as TeacherBoard } from "containers/board/teacherboard/teacherboard";
--- src/containers/class-router.jsx
+++ src/containers/class-router.jsx
@@ -11,14 +11,15 @@ import {
 
 //containers
 import {
-    MainBoard, 
-    NoticeBoard, 
-    WorkBoard, 
-    QnABoard, 
-    QnAPost, 
-    QnAWrite, 
-    LiveQuizBoard,
-    TeacherBoard
+  MainBoard,
+  NoticeBoard,
+  WorkBoard,
+  QnABoard,
+  QnAPost,
+  QnAWrite,
+  LiveQuizBoard,
+  LiveQuiz,
+  TeacherBoard
 } from "containers/board";
 import { TemplateContainer } from "containers";
 
@@ -59,59 +60,82 @@ class ClassRouter extends Component {
       <TeacherIcon />
     ];
 
-const ListItems = (text, index, URLs=["","/notice","/work","/qna","/livequiz","/teacher"]) => (
-			<ListItem 
-				button
-                component={Link}
-                to={`${match.url}${URLs[index]}`}
-            >
-                <ListItemIcon>
-                <SvgIcon>{boardIcons[index]}</SvgIcon>
-                </ListItemIcon>
-                <ListItemText 
-                inset 
-                primary={text}
-                />
-            </ListItem>
-        )
-    
+    const ListItems = (
+      text,
+      index,
+      URLs = ["", "/notice", "/work", "/qna", "/livequiz", "/teacher"]
+    ) => (
+      <ListItem button component={Link} to={`${match.url}${URLs[index]}`}>
+        <ListItemIcon>
+          <SvgIcon>{boardIcons[index]}</SvgIcon>
+        </ListItemIcon>
+        <ListItemText inset primary={text} />
+      </ListItem>
+    );
 
-     const drawer = (
-            <div>
-              {boardNames.map((text, index) => (
-                <div key = {index*10}>
-                  <Divider />
-                    {ListItems(text, index)}
-                </div>
-              ))}
-            </div>
-        );
-        return (
-            <TemplateContainer 
-                theme={theme}
-                drawer={drawer}
-                title='우효'
-                isLogin={true}
-                user={"user1"}
-            >
-                <Switch>
-                    <Route exact path={match.url} render={()=>(
-                        <MainBoard classIdx={match.params.id} boardIdx={0} token={token}/>
-                    )}/>
-                    <Route path={`${match.url}/notice`} render={()=>(
-                        <NoticeBoard classIdx={match.params.id} boardIdx={1} token={token}/>
-                    )}/>
-                    <Route path={`${match.url}/work`} render={()=>(
-                        <WorkBoard classIdx={match.params.id} boardIdx={2} token={token}/>
-                    )}/>
-                    <Route exact path={`${match.url}/qna`} render={()=>(
-                        <QnABoard classIdx={match.params.id} boardIdx={3} token={token}/>
-                    )}/>
-                    <Route path={`${match.url}/qna/write`} render={()=>(
-                        <QnAWrite classIdx={match.params.id} token={token}/>
-                    )}/>
-                    <Route path={`${match.url}/qna/post/:id`} component={QnAPost}/>
-                    <Route
+    const drawer = (
+      <div>
+        {boardNames.map((text, index) => (
+          <div key={index * 10}>
+            <Divider />
+            {ListItems(text, index)}
+          </div>
+        ))}
+      </div>
+    );
+    return (
+      <TemplateContainer
+        theme={theme}
+        drawer={drawer}
+        title="우효"
+        isLogin={true}
+        user={"user1"}
+      >
+        <Switch>
+          <Route
+            exact
+            path={match.url}
+            render={() => (
+              <MainBoard
+                classIdx={match.params.id}
+                boardIdx={0}
+                token={token}
+              />
+            )}
+          />
+          <Route
+            path={`${match.url}/notice`}
+            render={() => (
+              <NoticeBoard
+                classIdx={match.params.id}
+                boardIdx={1}
+                token={token}
+              />
+            )}
+          />
+          <Route
+            path={`${match.url}/work`}
+            render={() => (
+              <WorkBoard
+                classIdx={match.params.id}
+                boardIdx={2}
+                token={token}
+              />
+            )}
+          />
+          <Route
+            exact
+            path={`${match.url}/qna`}
+            render={() => (
+              <QnABoard classIdx={match.params.id} boardIdx={3} token={token} />
+            )}
+          />
+          <Route
+            path={`${match.url}/qna/write`}
+            render={() => <QnAWrite classIdx={match.params.id} token={token} />}
+          />
+          <Route path={`${match.url}/qna/post/:id`} component={QnAPost} />
+          <Route
             path={`${match.url}/livequiz/:id`}
             render={props => <LiveQuiz token={token} {...props} />}
           />
@@ -121,12 +145,19 @@ const ListItems = (text, index, URLs=["","/notice","/work","/qna","/livequiz","/
               <LiveQuizBoard boardIdx={4} token={token} match={match} />
             )}
           />
-                    <Route path={`${match.url}/teacher`} render={()=>(
-                        <TeacherBoard classIdx={match.params.id} boardIdx={5}  token={token}/>
-                    )}/> 
-                </Switch>
-            </TemplateContainer>            
-        );
-    }
+          <Route
+            path={`${match.url}/teacher`}
+            render={() => (
+              <TeacherBoard
+                classIdx={match.params.id}
+                boardIdx={5}
+                token={token}
+              />
+            )}
+          />
+        </Switch>
+      </TemplateContainer>
+    );
+  }
 }
 export default useAuth(ClassRouter);
